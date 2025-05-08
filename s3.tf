874b386c85f26422861e36166f21f612ae72ec1b
Narendra267 <narendrakolluri2672001@gmail.com>
2025-05-08 12:31:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,29 +1,9 @@
-terraform {
-  cloud {
-    organization = "swiggy_app"
-
-    workspaces {
-      name = "new-infra-3-tier"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1" # Or your preferred region
-}
-
+# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform"
 
+  # Enable versioning
   versioning {
     enabled = true
   }
 }
-
